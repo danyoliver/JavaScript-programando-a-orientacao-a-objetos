b3d6809e0d6986394bf6ed8407871da00a45d262
dany <danybatista046@gmail.com>
2021-03-10 11:16:13 -0300

    aula 03

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-class Cliente{
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this. _saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if (valor <= 0){
-            return;
-        }
-        this._saldo += valor
-    }
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo" ;
@@ -32,12 +11,14 @@ cliente2.cpf = 88822233309;
 
 const ContaCorrenteRicardo = new ContaCorrente();
 ContaCorrenteRicardo.agencia = 1001;
+ContaCorrenteRicardo.cliente = cliente1;
+ContaCorrenteRicardo.depositar(500);
 
+const conta2 = new ContaCorrente();
+conta2.agencia = 1002;
+conta2.cliente = cliente2;
 
-ContaCorrenteRicardo.depositar (100); 
+ContaCorrenteRicardo.transferir(200, conta2)
 
-
-const valorSacado = ContaCorrenteRicardo.sacar(50)
-console.log(valorSacado);
-
+console.log(conta2);
 console.log(ContaCorrenteRicardo);
